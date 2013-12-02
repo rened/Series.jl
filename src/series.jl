3dfc4cf46e226234dfc4a018bde9a685fdd52ce9
milktrader <milktrader@gmail.com>
2013-12-01 20:12:10 -0500

    removing na for now, playing around with types

--- src/series.jl
+++ src/series.jl
@@ -1,8 +1,4 @@
-using Datetime
-
 module A
-
-using Datetime
 
 import Base.show
 
@@ -14,12 +10,20 @@ export Series,
 
 abstract AbstractSeries
 
-type Series <: AbstractSeries
-  index
-  values
-  nas::BitArray
+immutable Series{T, V} <: AbstractSeries
+  index::T
+  value::V
 
-  Series(x,y) = new(x,y,falses(length(x)))
+#  if length(x == y)
+#  Series(x,y) = new(x,y)
+#  else
+#  "Must be the same length"
+#  end
+#  end
+end
+
+type DataSeries{T,V} <: AbstractSeries
+  x::Array{Series{T,V},1}
 end
 
 #################################
@@ -27,24 +31,30 @@ end
 #################################
 
 function show(io::IO, p::Series)
-  n = length(p.index)
-    if n < 7
-  for i = 1:n
-      println(io, "  ", p.index[i], "  ", p.values[i])
-    end
-  end
-    if n > 7
-    for i = 1:3
-    println(io, "  ", p.index[i], "  ", p.values[i])
-    end
-    println("  ...")
-    println("  ... extra exciting stuff is here!")
-    println("  ...")
-    for i = n-2:n
-    println(io, "  ", p.index[i], "  ", p.values[i])
-    end
-  end
+   #print(io, p.index, "  |  ", join([@sprintf("%.4f",x) for x in p.value]," "))
+   print(io, p.index, " | ",  p.value)
+
 end
+
+# function show(io::IO, p::Series)
+#   n = length(p.index)
+#     if n < 7
+#   for i = 1:n
+#       println(io, "  ", p.index[i], "  ", p.values[i])
+#     end
+#   end
+#     if n > 7
+#     for i = 1:3
+#     println(io, "  ", p.index[i], "  ", p.values[i])
+#     end
+#     println("  ...")
+#     println("  ... extra exciting stuff is here!")
+#     println("  ...")
+#     for i = n-2:n
+#     println(io, "  ", p.index[i], "  ", p.values[i])
+#     end
+#   end
+# end
 
 #################################
 ###### head, tail ###############
