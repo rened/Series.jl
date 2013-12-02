9878b7eb52b4524d651b21227c790007f6e51f51
milktrader <milktrader@gmail.com>
2013-12-01 20:36:18 -0500

    showing DataSeries

--- src/series.jl
+++ src/series.jl
@@ -3,6 +3,7 @@ module A
 import Base.show
 
 export Series, 
+       DataSeries,
        head,
        tail,
        first, 
@@ -13,17 +14,10 @@ abstract AbstractSeries
 immutable Series{T, V} <: AbstractSeries
   index::T
   value::V
-
-#  if length(x == y)
-#  Series(x,y) = new(x,y)
-#  else
-#  "Must be the same length"
-#  end
-#  end
 end
 
 type DataSeries{T,V} <: AbstractSeries
-  x::Array{Series{T,V},1}
+  collection::Array{Series{T,V},1}
 end
 
 #################################
@@ -36,31 +30,32 @@ function show(io::IO, p::Series)
 
 end
 
-# function show(io::IO, p::Series)
-#   n = length(p.index)
-#     if n < 7
-#   for i = 1:n
-#       println(io, "  ", p.index[i], "  ", p.values[i])
-#     end
-#   end
-#     if n > 7
-#     for i = 1:3
-#     println(io, "  ", p.index[i], "  ", p.values[i])
-#     end
-#     println("  ...")
-#     println("  ... extra exciting stuff is here!")
-#     println("  ...")
-#     for i = n-2:n
-#     println(io, "  ", p.index[i], "  ", p.values[i])
-#     end
-#   end
-# end
+function show(io::IO, p::DataSeries)
+  n = length(p.collection)
+    if n < 7
+  for i = 1:n
+      println(io, p.collection[i])
+    end
+  end
+    if n > 7
+    for i = 1:3
+    println(io, p.collection[i])
+    end
+    println("  ...")
+    println("  ... extra stuff is here!")
+    println("  ...")
+    for i = n-2:n
+    println(io, p.collection[i])
+    end
+  end
+end
 
 #################################
 ###### head, tail ###############
 #################################
 
 head{T<:Series}(x::Array{T}, n::Int) = x[1:n]
+head{T<:DataSeries}(x::T, n::Int) = x[1:n]
 head{T<:Series}(x::Array{T}) = head(x, 3)
 first{T<:Series}(x::Array{T}) = head(x, 1)
 
