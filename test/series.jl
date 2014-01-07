ec58e41670385eff08e6dd5228b0f6a797ebc4cd
milktrader <milktrader@gmail.com>
2014-01-07 16:31:16 -0500

    series passing now

--- test/series.jl
+++ test/series.jl
@@ -11,15 +11,17 @@ jtest( (op[1] + op[1]).value == 211.52,
        (op[1] - op[1]).value == 0,
        (op[1] * op[1]).value == 11185.1776,
        (op[1] / op[1]).value == 1,
-        op[1] / op[2] == nothing,                     # since indexes don't match, nothing is returned
-        op[1] < op[2] == nothing)  # not sure why this works 
+        op[1] / op[2]        == nothing,  # since indexes don't match, nothing is returned
+        eval(op[1] < op[2])  == true)     # this should be nothing since indexes don't match?
 fails += f
      
 @context "dates are correct"
-jtest(op[1].index == firstday)
+jtest(op[1].index  == firstday,
+      op[2].index  == secondday,
+      op[10].index == tenthday)
 fails += f
 
 @context "types are correct"
-jtest(typeof(op) == ArraySeriesPairDateFloat64,
+jtest(typeof(op)    == ArraySeriesPairDateFloat64,
       typeof(op[1]) == SeriesPairDateFloat64)
 fails += f
