0a7e92b040e1d3a119268cf1af5deb6a72d1cdbf
milktrader <milktrader@gmail.com>
2014-01-09 11:06:08 -0500

    switched to using FactCheck testing framework

--- test/array.jl
+++ test/array.jl
@@ -1,65 +1,67 @@
-using MarketData
+using MarketData, FactCheck
 
-fails = 0
+nam = SeriesArray([1:3], [2:4], "test")
+boo = SeriesArray([1:3], trues(3))
+arr = Array(op, cl[2:end])
+
+facts("Array") do
   
-@context "boolean values are evaluated"
-ba = SeriesArray([1:3], trues(3))
-f=jtest( sum(value(ba)) == 3)
-fails += f
-
-@context "arrays with names"
-na = SeriesArray([1:3], [2:4], "test")
-f=jtest(na[1].name == "test", 
-        na[2].name == "test", 
-        na[3].name == "test")
-fails += f
-
-@context "readseries sorts"
-f=jtest( op[1].value == 105.76,             
-         op[1].index == firstday)
-fails += f
+  context("boolean values are evaluated") do
+    @fact sum(value(boo)) => 3
+  end
   
-@context "indexing"
-f=jtest(op[1].index       == firstday,
-        op[1].value       == 105.76, 
-        length(op[2:end]) == 504)  
-fails += f
-
-@context "construct Array of values"
-arr    = Array(op, cl[2:end])
-f=jtest(size(arr)                    == (505,2),
-        round(sum(arr[2:end,2]), 2)  == 62216.27,
-        typeof(arr)                  == Array{Float64, 2},
-        isnan(arr[1,2])              == true)
-fails += f
-
-@context "remove rows that have NaN"
-f=jtest( size(removenan(arr))                   == (504,2), 
-         isnan(sum(removenan(arr)))             == false, 
-         length(lag(op))                        == 505, 
-         isnan(sum([v.value for v in lag(op)])) == true)
-fails += f
-
-@context "broadcast operator needs rewrite" 
-# "CAUTION: sorting NOT enforced"
-# "TODO: enforce sorting"
-f=jtest((op .+ cl)[1].value == 210.98000000000002, 
-        (op .- cl)[1].value ==  0.5400000000000063, 
-        (op .* cl)[1].value ==  11128.067200000001,   
-        (op ./ cl)[1].value ==  1.0051321041627068)
-fails += f
-
-@context "head and tail"
-f=jtest(length(head(op))  == 1, 
-        length(tail(op)) == 504, 
-        head(op)[1].value == 105.76)
-fails += f
-
-@context "lag and lead sorted array"
-f=jtest(isnan(lag(op)[1].value) == true, 
-        lag(op)[2].value        == 105.76,
-        lag(op,2)[3].value      == 105.76,
-        lag(op)[1].index        == firstday,
-        lead(op)[1].value       == 105.22, 
-        lead(op)[1].index       == firstday)
-fails += f
+  context( "arrays with nammes") do
+    @fact nam[1].name => "test" 
+    @fact nam[2].name => "test" 
+    @fact nam[3].name => "test"
+  end
+   
+   context("readseries sorts") do
+     @fact op[1].value => 105.76             
+     @fact op[1].index => firstday
+   end
+     
+   context("indexing") do
+     @fact op[1].index       => firstday
+     @fact op[1].value       => 105.76
+     @fact length(op[2:end]) => 504  
+   end
+   
+  context("construct Array of values") do
+    @fact size(arr)                    => (505,2)
+    @fact round(sum(arr[2:end,2]), 2)  => 62216.27
+    @fact typeof(arr)                  => Array{Float64, 2}
+    @fact isnan(arr[1,2])              => true
+  end
+  
+  context("remove rows that have namN") do
+    @fact size(removenan(arr))                   => (504,2) 
+    @fact isnan(sum(removenan(arr)))             => false 
+    @fact length(lag(op))                        => 505 
+    @fact isnan(sum([v.value for v in lag(op)])) => true
+  end
+  
+  context("broadcast operator needs rewrite") do
+  # "CAUTION: sorting NOT enforced"
+  # "TODO: enforce sorting"
+    @fact (op .+ cl)[1].value =>  210.98000000000002 
+    @fact (op .- cl)[1].value =>  0.5400000000000063 
+    @fact (op .* cl)[1].value =>  11128.067200000001   
+    @fact (op ./ cl)[1].value =>  1.0051321041627068
+  end
+  
+  context("head and tail") do
+    @fact length(head(op))  => 1 
+    @fact length(tail(op))  => 504 
+    @fact head(op)[1].value => 105.76
+  end
+  
+  context("lag and lead sorted array") do
+    @fact isnan(lag(op)[1].value) => true 
+    @fact lag(op)[2].value        => 105.76
+    @fact lag(op,2)[3].value      => 105.76
+    @fact lag(op)[1].index        => firstday
+    @fact lead(op)[1].value       => 105.22 
+    @fact lead(op)[1].index       => firstday
+  end
+end
--- test/utilities.jl
+++ test/utilities.jl
@@ -1,24 +1,27 @@
-using MarketData
+using MarketData, FactCheck
 
-fails = 0
- 
-@context "value, index and name"
-f=jtest(value(op)[1] == 105.76, 
-        index(op)[1] == firstday, 
-        name(op)[1] == name(op)[2] == name(op)[3])
-fails += f
-
-@context "istrue"
 bt = SeriesArray([1:3], trues(3))
 ba = push!(bt, SeriesPair(4, false))
-f=jtest(length(istrue(ba)) == 3)
-fails += f
 
-@context "when"
-f=jtest(when(op, [firstday, secondday, tenthday])[1].value == 105.76, 
-        when(op, [firstday, secondday, tenthday])[2].value == 105.22,
-        when(op, [firstday, secondday, tenthday])[3].value == 111.14,
-        when(op, [firstday, secondday, tenthday])[1].index == firstday,
-        when(op, [firstday, secondday, tenthday])[2].index == secondday,
-        when(op, [firstday, secondday, tenthday])[3].index == tenthday)
-fails += f
+facts("Utilities") do
+
+  context("value, index and name") do
+    @fact value(op)[1] => 105.76 
+    @fact index(op)[1] => firstday 
+    @fact name(op)[1]  => name(op)[2] 
+    @fact name(op)[2]  => name(op)[3] 
+  end
+  
+  context("istrue") do
+    @fact length(istrue(ba)) => 3
+  end
+  
+  context("when") do
+    @fact when(op, [firstday, secondday, tenthday])[1].value => 105.76 
+    @fact when(op, [firstday, secondday, tenthday])[2].value => 105.22
+    @fact when(op, [firstday, secondday, tenthday])[3].value => 111.14
+    @fact when(op, [firstday, secondday, tenthday])[1].index => firstday
+    @fact when(op, [firstday, secondday, tenthday])[2].index => secondday
+    @fact when(op, [firstday, secondday, tenthday])[3].index => tenthday
+  end
+end
